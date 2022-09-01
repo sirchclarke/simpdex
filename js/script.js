db890d29fb7c097a130ee7657fa5a818fb20aebd
Chris Clarke-Gonzalez <105073124+sirchclarke@users.noreply.github.com>
2022-09-01 06:44:30 -0700

    pokeddex array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-alert('Hello world');
-let favoriteFood = 'italian';
-document.write(favoriteFood);
+let pokemonList = [ //array of objects below
+{
+    name : 'Zubat',
+    hp : 40.0,
+    types : ['poison', 'flying']
+},
+{
+    name : 'Ninetails',
+    hp : 73.0,
+    types : ['ice', 'fairy']
+},
+{
+    name : 'Nidoking',
+    hp : 81.0,
+    types : ['poison', 'flying']
+},
+{
+    name : 'Venomoth',
+    hp : 70.0,
+    types : ['bug', 'poison']
+},
+{
+    name : 'Parasect',
+    hp : 60.0,
+    types : ['bug', 'grass']
+}
+]
